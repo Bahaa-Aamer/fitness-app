cc2edea8f5dc7fd64d23a752e4c6277970724d79
Bahaa Aamer <bhaa.amer1.ba@gmail.com>
2024-02-02 01:52:31 +0200

    -

---
+++ js/fitnees.academy.js
@@ -0,0 +1,83 @@
+
+
+
+
+// legends section
+
+
+$(document).ready(function(){
+    $('.simeon').hover(function(){
+        $('.cap1').toggle();
+    });
+ 
+});
+
+
+
+$(document).ready(function(){
+    $('.rana').hover(function(){
+        $('.cap2').toggle();
+    });
+ 
+});
+
+
+
+$(document).ready(function(){
+    $('.jeff').hover(function(){
+        $('.cap3').toggle();
+    });
+ 
+});
+
+
+
+$(document).ready(function(){
+    $('.lazar').hover(function(){
+        $('.cap4').toggle('slow');
+    });
+ 
+});
+
+
+$(document).ready(function(){
+    $('.ulisses').hover(function(){
+        $('.cap5').toggle();
+    });
+ 
+});
+
+
+
+$(document).ready(function(){
+    $('.sally').hover(function(){
+        $('.cap6').toggle();
+    });
+ 
+});
+
+
+
+
+// login section 
+
+
+$(document).ready(function(){
+    $('.login').hover(function(){
+        $('.login-contain').toggle();
+
+
+    });
+});
+
+
+
+
+
+//  slkasaks;akp
+
+$(document).ready(function(){
+    $('.sixth').hover(function(){
+        alert("hover on 'start your journey'");
+    });
+});
